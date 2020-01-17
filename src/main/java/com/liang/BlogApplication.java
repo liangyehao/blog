9c690f381892410ecd123a64e3169eb037a54d92
liangyehao <109431109@qq.com>
2020-01-17 15:49:15 +0800

    空项目，整合了web模块

---
+++ src/main/java/com/liang/BlogApplication.java
@@ -0,0 +1,18 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangyehao
+ * @date 2020-01-17 15:42
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
