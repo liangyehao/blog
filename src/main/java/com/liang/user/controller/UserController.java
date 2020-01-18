42ce104e3247748cf409bcf0f9bd6225c904e260
liangyehao <1094311509@qq.com>
2020-01-18 16:53:18 +0800

    完善了用户的增删查改功能与页面

--- src/main/java/com/liang/user/controller/UserController.java
+++ src/main/java/com/liang/user/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.liang.user.controller;
 
+import com.liang.user.domain.User;
 import com.liang.user.repositotry.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -33,5 +31,33 @@ public class UserController {
         model.addAttribute("user", userRepository.findUser(id));
         model.addAttribute("title", "用户详情");
         return new ModelAndView("user/view", "userModel", model);
+    }
+
+    @GetMapping("/modify/{id}")
+    public ModelAndView creatForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userRepository.findUser(id));
+        model.addAttribute("title", "修改用户");
+        return new ModelAndView("user/form", "userModel", model);
+    }
+
+    @PostMapping
+    public ModelAndView saveOrUpdateUser(User user, Model model) {
+        model.addAttribute("user", userRepository.saveOrUpdateUser(user));
+        model.addAttribute("title", "用户列表");
+        return new ModelAndView("redirect:/users", "userModel", model);
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteUser(@PathVariable("id") Long id, Model model) {
+        userRepository.deleteUser(id);
+        model.addAttribute("title", "用户列表");
+        return new ModelAndView("redirect:/users", "userModel", model);
+    }
+
+    @GetMapping("/create")
+    public ModelAndView createForm(User user, Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "创建新用户");
+        return new ModelAndView("user/form", "userModel", model);
     }
 }
